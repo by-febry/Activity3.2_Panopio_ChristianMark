479a92d8f319b812e5a7e1ba8bc44e44836af586
by-febry <undestiny2627@gmail.com>
2023-10-26 23:00:29 +0800

    feat update delete inactive

--- hero.sql
+++ hero.sql
@@ -152,6 +152,10 @@ VALUES
     (9, 9, 109),  -- Leomord has Blade Armor
     (10, 10, 110);  -- Chou has Feather of Heaven
 
-UPDATE heroes
+ UPDATE heroes
 SET state = 'inactive'
 WHERE hero_id = 1;
+
+DELETE FROM items
+WHERE hero_id = 1;
+  
